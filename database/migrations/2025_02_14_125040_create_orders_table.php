5918ee160b7ce7f198ebb07497b86c6dbeac8363
overdose. <onyoux755@gmail.com>
2025-02-20 23:17:18 +0700

    เบื่อเหงาอยากโดนเหลากระบาน

---
+++ database/migrations/2025_02_14_125040_create_orders_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+{
+    if (!Schema::hasTable('orders')) {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('OrderStatus')->default('pending');
+            $table->timestamp('OrderDate')->useCurrent();
+            $table->decimal('TotalAmount', 10, 2);
+            $table->timestamps();
+        });
+    }
+}
+
+
+    public function down() {
+        Schema::dropIfExists('orders');
+    }
+};
